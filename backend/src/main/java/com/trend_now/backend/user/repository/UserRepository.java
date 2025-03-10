0e4820c143de8f7125bbbdd11fa217747b46c655
chanwoong <cksdnddlek99@gmail.com>
2025-03-10 21:56:38 +0900

    chore: User 클래스 unique 옵션을 uniqueConstraints로 변경

--- backend/src/main/java/com/trend_now/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/trend_now/backend/user/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.trend_now.backend.user.repository;
 
 
-import com.trend_now.backend.user.domain.Users;
+import com.trend_now.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
