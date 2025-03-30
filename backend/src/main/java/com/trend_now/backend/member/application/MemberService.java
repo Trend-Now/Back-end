b9192ab49a1b395602efdb2d6004ef91e0a1b584
chanwoong <cksdnddlek99@gmail.com>
2025-03-30 17:31:14 +0900

    chore: 클래스 레벨에서 Transactional(readOnly = true) 설정, 쓰기 작업에서는 Transactional 어노테이션 처리

--- backend/src/main/java/com/trend_now/backend/member/application/MemberService.java
+++ backend/src/main/java/com/trend_now/backend/member/application/MemberService.java
@@ -4,14 +4,14 @@ import com.trend_now.backend.member.data.vo.GoogleProfile;
 import com.trend_now.backend.member.domain.Members;
 import com.trend_now.backend.member.domain.Provider;
 import com.trend_now.backend.member.repository.MemberRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Slf4j
 public class MemberService {
@@ -19,6 +19,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public Members createOauth(GoogleProfile googleProfile, Provider provider) {
         Members member = Members.builder()
                 .email(googleProfile.getEmail())
