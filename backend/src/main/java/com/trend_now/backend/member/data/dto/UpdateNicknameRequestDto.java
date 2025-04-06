5430ec6369d3979d05d778930cfa33ee5afd1293
WooyoungKwon <rnjsdndud00@naver.com>
2025-04-06 22:54:20 +0900

    [CCS-26] refactor: UpdateNicknameRequestDto에 Valid 값 추가

--- backend/src/main/java/com/trend_now/backend/member/data/dto/UpdateNicknameRequestDto.java
+++ backend/src/main/java/com/trend_now/backend/member/data/dto/UpdateNicknameRequestDto.java
@@ -1,6 +1,12 @@
 package com.trend_now.backend.member.data.dto;
 
-public record UpdateNicknameRequestDto (
-        String nickname
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateNicknameRequestDto(
+    @NotBlank(message = "닉네임은 필수입니다.")
+    @Size(max = 20, message = "닉네임은 20자 이내로만 가능합니다.")
+    String nickname
 ) {
+
 }
