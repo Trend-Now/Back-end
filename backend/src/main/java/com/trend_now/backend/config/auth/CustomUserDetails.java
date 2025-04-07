936898c49c769ce88a4a22f4c3311f8e1a3438b3
Chanwoong <cksdnddlek99@gmail.com>
2025-04-07 17:39:05 +0900

    CCS-26 chore: 일관성에 맞게 UserDetails 객체의 username 값을 memberId로 반환

--- backend/src/main/java/com/trend_now/backend/config/auth/CustomUserDetails.java
+++ backend/src/main/java/com/trend_now/backend/config/auth/CustomUserDetails.java
@@ -44,7 +44,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return members.getSnsId();
+        return String.valueOf(members.getId());
     }
 
     /**
