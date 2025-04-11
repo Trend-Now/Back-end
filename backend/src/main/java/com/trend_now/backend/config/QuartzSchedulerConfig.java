10440bb6afd83817204650ecc6984106830861cb
choi <nippert@naver.com>
2025-04-11 21:33:04 +0900

    CCS-28 feat: Redis 좋아요 수를 DB로 동기화하는 작업을 일정 주기로 실행하는 기능 구성

--- backend/src/main/java/com/trend_now/backend/config/QuartzSchedulerConfig.java
+++ backend/src/main/java/com/trend_now/backend/config/QuartzSchedulerConfig.java
@@ -2,6 +2,8 @@ package com.trend_now.backend.config;
 
 import com.trend_now.backend.board.application.SignalKeywordJob;
 import com.trend_now.backend.board.application.SignalKeywordJobListener;
+import com.trend_now.backend.post.application.PostLikesSyncDbJob;
+import com.trend_now.backend.post.application.PostLikesSyncDbJobListener;
 import jakarta.annotation.PostConstruct;
 import org.quartz.JobBuilder;
 import org.quartz.JobDataMap;
@@ -17,18 +19,22 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(name = "signalKeywordScheduler.enabled", havingValue = "true", matchIfMissing = false)
-public class SignalKeywordSchedulerConfig {
+@ConditionalOnProperty(name = "quartzScheduler.enabled", havingValue = "true", matchIfMissing = false)
+public class QuartzSchedulerConfig {
 
     private static final String SIGNAL_KEYWORD_GROUP = "SignalGroup";
     private static final String SIGNAL_KEYWORD_JOB = "SignalKeywordJob";
     private static final String SIGNAL_KEYWORD_TRIGGER = "SignalKeywordTrigger";
     private static final int SIGNAL_KEYWORD_SCHEDULER_INTERVAL_SECONDS = 11;
+    private static final String POST_LIKES_SYNC_DB_JOB = "PostLikesSyncDbJob";
+    private static final String POST_LIKES_SYNC_DB_JOB_GROUP = "PostLikesSyncDbJobGroup";
+    private static final String POST_LIKES_SYNC_DB_JOB_TRIGGER = "PostLikesSyncDbJobTrigger";
+    private static final int POST_LIKES_SYNC_DB_JOB_INTERVAL_SECONDS = 11;
 
     private Scheduler scheduler;
     private final ApplicationContext applicationContext;
 
-    public SignalKeywordSchedulerConfig(Scheduler scheduler,
+    public QuartzSchedulerConfig(Scheduler scheduler,
             ApplicationContext applicationContext) {
         this.scheduler = scheduler;
         this.applicationContext = applicationContext;
@@ -58,10 +64,37 @@ public class SignalKeywordSchedulerConfig {
                                 .repeatForever())
                 .build();
 
+        JobDataMap postLikesCtx = new JobDataMap();
+        postLikesCtx.put("applicationContext", applicationContext);
+
+        JobDetail postLikesJob = JobBuilder
+                .newJob(PostLikesSyncDbJob.class)
+                .withIdentity(POST_LIKES_SYNC_DB_JOB, POST_LIKES_SYNC_DB_JOB_GROUP)
+                .withDescription("DB 동기화 job")
+                .setJobData(postLikesCtx)
+                .build();
+
+        Trigger postLikesTrigger = TriggerBuilder
+                .newTrigger()
+                .withIdentity(POST_LIKES_SYNC_DB_JOB_TRIGGER, POST_LIKES_SYNC_DB_JOB_GROUP)
+                .withDescription("DB 동기화 Trigger")
+                .startNow()
+                .withSchedule(
+                        SimpleScheduleBuilder
+                                .simpleSchedule()
+                                .withIntervalInSeconds(POST_LIKES_SYNC_DB_JOB_INTERVAL_SECONDS)
+                                .repeatForever())
+                .build();
+
         scheduler = new StdSchedulerFactory().getScheduler();
+
         SignalKeywordJobListener signalKeywordJobListener = new SignalKeywordJobListener();
+        PostLikesSyncDbJobListener postLikesSyncDbJobListener = new PostLikesSyncDbJobListener();
         scheduler.getListenerManager().addJobListener(signalKeywordJobListener);
+        scheduler.getListenerManager().addJobListener(postLikesSyncDbJobListener);
+
         scheduler.start();
         scheduler.scheduleJob(job, trigger);
+        scheduler.scheduleJob(postLikesJob, postLikesTrigger);
     }
 }
