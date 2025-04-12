94a5777211bdf6f7d8c2ff6bc8335b8d1f19a87e
chanwoong <cksdnddlek99@gmail.com>
2025-04-12 15:30:42 +0900

    CCS-31 feat: 댓글 테이블에 "board_ttl_status" enum 타입 컬럼 추가

---
+++ backend/src/main/java/com/trend_now/backend/comment/domain/BoardTtlStatus.java
@@ -0,0 +1,6 @@
+package com.trend_now.backend.comment.domain;
+
+public enum BoardTtlStatus {
+
+    BOARD_TTL_BEFORE, BOARD_TTL_AFTER
+}
--- backend/src/main/java/com/trend_now/backend/comment/domain/Comments.java
+++ backend/src/main/java/com/trend_now/backend/comment/domain/Comments.java
@@ -2,14 +2,7 @@ package com.trend_now.backend.comment.domain;
 
 import com.trend_now.backend.config.domain.BaseEntity;
 import com.trend_now.backend.post.domain.Posts;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,4 +30,9 @@ public class Comments extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Posts posts;
+
+    // BOARD_TTL 시간 안에 작성된 댓글은 boardTtlStatus 필드가 BOARD_TTL_BEFORE, 아니면 BOARD_TTL_AFTER 할당
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "board_ttl_status")
+    private BoardTtlStatus boardTtlStatus;
 }
