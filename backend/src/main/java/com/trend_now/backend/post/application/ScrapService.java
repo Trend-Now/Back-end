0245dd0eb5bc5c8764b5f25fc368e0144e103119
WooyoungKwon <rnjsdndud00@naver.com>
2025-04-17 15:30:33 +0900

    CCS-35 feat(scrap): 디자인에 맞는 요구사항으로 변경

--- backend/src/main/java/com/trend_now/backend/post/application/ScrapService.java
+++ backend/src/main/java/com/trend_now/backend/post/application/ScrapService.java
@@ -1,10 +1,8 @@
 package com.trend_now.backend.post.application;
 
 import com.trend_now.backend.post.domain.Posts;
-import com.trend_now.backend.post.domain.Scraps;
-import com.trend_now.backend.post.dto.PostsInfoDto;
+import com.trend_now.backend.post.dto.PostListDto;
 import com.trend_now.backend.post.repository.ScrapRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,19 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScrapService {
 
     private final ScrapRepository scrapRepository;
+    private final PostLikesService postLikesService;
 
-    public List<PostsInfoDto> getScrappedPostsByMemberId(Long memberId, int page, int size) {
+    public Page<PostListDto> getScrappedPostsByMemberId(Long memberId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
-        Page<Scraps> scraps = scrapRepository.findScrapsByMembers_Id(memberId, pageable);
-        return scraps.stream()
-            .map(scrap -> {
-                Posts posts = scrap.getPosts();
-                return PostsInfoDto.builder()
-                    .title(posts.getTitle())
-                    .writer(posts.getWriter())
-                    .viewCount(posts.getViewCount())
-                    .build();
-            })
-            .toList();
+        Page<Posts> scrapPosts = scrapRepository.findScrapPostsByMemberId(memberId, pageable);
+        return scrapPosts.map(posts -> {
+            int postLikesCount = postLikesService.getPostLikesCount(posts.getBoards().getId(),
+                posts.getId());
+            return PostListDto.of(posts, postLikesCount);
+        });
     }
 }
