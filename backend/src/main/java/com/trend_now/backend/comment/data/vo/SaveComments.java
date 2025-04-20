05649d300f26982c81c09a3f21492618665eea50
chanwoong <cksdnddlek99@gmail.com>
2025-04-20 17:12:07 +0900

    CCS-31 chore: Builder 패턴 추가

--- backend/src/main/java/com/trend_now/backend/comment/data/vo/SaveComments.java
+++ backend/src/main/java/com/trend_now/backend/comment/data/vo/SaveComments.java
@@ -2,10 +2,12 @@ package com.trend_now.backend.comment.data.vo;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Builder
 public class SaveComments {
 
     /**
