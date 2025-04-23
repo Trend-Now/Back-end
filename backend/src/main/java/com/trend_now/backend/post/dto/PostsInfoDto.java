5a30f30ef43718b522f174fc404ae810f4f69eed
WooyoungKwon <rnjsdndud00@naver.com>
2025-04-23 23:42:15 +0900

    CCS-35 refactor(post): 프론트에 반환되는 DTO에 좋아요 개수 추가

--- backend/src/main/java/com/trend_now/backend/post/dto/PostsInfoDto.java
+++ backend/src/main/java/com/trend_now/backend/post/dto/PostsInfoDto.java
@@ -5,6 +5,7 @@ package com.trend_now.backend.post.dto;
 
 import com.trend_now.backend.image.dto.ImageInfoDto;
 import com.trend_now.backend.post.domain.Posts;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,10 +20,12 @@ public class PostsInfoDto {
     private String writer;
     private String content;
     private int viewCount;
+    private int likeCount;
     private List<ImageInfoDto> imageInfos;
+    private LocalDateTime updatedAt;
 
-    public static PostsInfoDto of(Posts posts, List<ImageInfoDto> imageInfos) {
+    public static PostsInfoDto of(Posts posts, int likeCount, List<ImageInfoDto> imageInfos) {
         return new PostsInfoDto(posts.getTitle(), posts.getWriter(), posts.getContent(),
-                posts.getViewCount(), imageInfos);
+                posts.getViewCount(), likeCount, imageInfos, posts.getUpdatedAt());
     }
 }
