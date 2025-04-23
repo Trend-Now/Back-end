b6b77ccfd645453a882cc1c98a427626f0838a96
WooyoungKwon <rnjsdndud00@naver.com>
2025-04-23 23:38:33 +0900

    CCS-35 feat(image): 이미지 조회, 삭제 관련 JPQL 작성

--- backend/src/main/java/com/trend_now/backend/image/repository/ImagesRepository.java
+++ backend/src/main/java/com/trend_now/backend/image/repository/ImagesRepository.java
@@ -3,11 +3,24 @@ package com.trend_now.backend.image.repository;
 import com.trend_now.backend.image.domain.Images;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImagesRepository extends JpaRepository<Images, Long> {
 
-    List<Images> findAllByPosts_Id(Long postsId);
+    @Query("SELECT i.s3Key FROM Images i WHERE i.posts.id = :postId")
+    List<String> findS3KeyByPostsId(Long postId);
 
+    @Query("SELECT i.s3Key FROM Images i WHERE i.id IN :ids")
+    List<String> findS3KeyByIdIn(List<Long> ids);
+
+    @Modifying
+    @Query("DELETE FROM Images i WHERE i.id IN :ids")
+    void deleteAllByIdIn(List<Long> ids);
+
+    void deleteAllByPosts_Id(Long postsId);
+
+    List<Images> findAllByPosts_Id(Long postsId);
 }
