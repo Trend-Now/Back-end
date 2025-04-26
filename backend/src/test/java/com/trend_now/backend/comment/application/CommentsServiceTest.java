dadc5157056a9e8fd1c0b2518e4713cb8dca856f
chanwoong <cksdnddlek99@gmail.com>
2025-04-26 16:41:37 +0900

    CCS-31 test: 댓글 삭제 API 테스트 구현

--- backend/src/test/java/com/trend_now/backend/comment/application/CommentsServiceTest.java
+++ backend/src/test/java/com/trend_now/backend/comment/application/CommentsServiceTest.java
@@ -6,10 +6,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.trend_now.backend.board.domain.BoardCategory;
 import com.trend_now.backend.board.domain.Boards;
 import com.trend_now.backend.board.repository.BoardRepository;
+import com.trend_now.backend.comment.data.vo.DeleteComments;
 import com.trend_now.backend.comment.data.vo.SaveComments;
 import com.trend_now.backend.comment.domain.BoardTtlStatus;
 import com.trend_now.backend.comment.domain.Comments;
 import com.trend_now.backend.comment.repository.CommentsRepository;
+import com.trend_now.backend.exception.CustomException.BoardTtlException;
 import com.trend_now.backend.exception.CustomException.NotFoundException;
 import com.trend_now.backend.member.domain.Members;
 import com.trend_now.backend.member.domain.Provider;
@@ -115,35 +117,28 @@ class CommentsServiceTest {
 
     @Test
     @DisplayName("BOARD_TTL 존재 여부에 따라 boardTtlStatus가 다르게 저장된다")
-    void checkBoardTtlStatusChange() {
+    void BOARD_TTL_여부에_따라_boardTtlStatus가_결정() {
         // given
         // redis에 게시판 저장
         String key = testBoards.getName() + BOARD_KEY_DELIMITER + testPost.getId();
-        redisTemplate.opsForValue().set(key, "1");
+        redisTemplate.opsForValue().set(key, "실시간 게시판");
 
-        SaveComments beforeSaveComments = SaveComments.builder()
+        SaveComments testSaveComments = SaveComments.builder()
                 .postId(testPost.getId())
                 .boardId(testPost.getId())
                 .boardName(testBoards.getName())
-                .content("BOARD_TTL_BEFORE testContent")
-                .build();
-
-        SaveComments afterSaveComments = SaveComments.builder()
-                .postId(testPost.getId())
-                .boardId(testPost.getId())
-                .boardName(testBoards.getName())
-                .content("BOARD_TTL_AFTER testContent")
+                .content("testContent")
                 .build();
 
         // when
         // BOARD_TTL 존재 했을 때, 댓글을 작성
-        commentsService.saveComments(testMembers, beforeSaveComments);
+        commentsService.saveComments(testMembers, testSaveComments);
 
         // Redis에서 키 삭제하여 TTL 없는 상태로 만듦
         redisTemplate.delete(key);
 
         // BOARD_TTL 존재하지 않았을 때, 댓글을 작성
-        commentsService.saveComments(testMembers, afterSaveComments);
+        commentsService.saveComments(testMembers, testSaveComments);
 
         // then
         List<Comments> commentsList = commentsRepository.findAll();
@@ -162,5 +157,59 @@ class CommentsServiceTest {
         // afterTtlComment의 BoardTtlStatus는 "BOARD_TTL_AFTER" 값을 가짐
         assertThat(afterTtlComments.getBoardTtlStatus()).isEqualTo(BoardTtlStatus.BOARD_TTL_AFTER);
 
+    }
+
+    @Test
+    @DisplayName("BOARD_TTL 존재 여부에 따라 댓글 삭제가 결정된다")
+    void BOARD_TTL_여부에_따라_댓글_삭제_결정() {
+        // given
+        // redis에 게시판 저장 및 댓글 2개 저장
+        String key = testBoards.getName() + BOARD_KEY_DELIMITER + testPost.getId();
+        redisTemplate.opsForValue().set(key, "실시간 게시판");
+
+        SaveComments testSaveComments = SaveComments.builder()
+                .postId(testPost.getId())
+                .boardId(testPost.getId())
+                .boardName(testBoards.getName())
+                .content("testContent")
+                .build();
+
+        commentsService.saveComments(testMembers, testSaveComments);
+        commentsService.saveComments(testMembers, testSaveComments);
+
+        List<Comments> commentsList = commentsRepository.findAll();
+        Comments comments1 = commentsList.get(0);
+        Comments comments2 = commentsList.get(1);
+
+        DeleteComments deleteComments1 = DeleteComments.builder()
+                .postId(testPost.getId())
+                .boardId(testBoards.getId())
+                .boardName(testSaveComments.getBoardName())
+                .commentId(comments1.getId())
+                .build();
+
+        DeleteComments deleteComments2 = DeleteComments.builder()
+                .postId(testPost.getId())
+                .boardId(testBoards.getId())
+                .boardName(testSaveComments.getBoardName())
+                .commentId(comments2.getId())
+                .build();
+
+
+        // when & then
+        // 하나는 BOARD_TTL 전에 삭제하여 삭제됨을 확인 & 나머지 하나는 BOARD_TTL 후에 삭제하지만 삭제안됨을 확인
+        commentsService.deleteCommentsByCommentId(testMembers, deleteComments1);
+
+        // Redis에서 키 삭제하여 TTL 없는 상태로 만듦
+        redisTemplate.delete(key);
+
+        assertThatThrownBy(() -> commentsService.deleteCommentsByCommentId(testMembers, deleteComments2))
+                .isInstanceOf(BoardTtlException.class)
+                .hasMessage("게시판 활성 시간이 만료되었습니다.");
+
+        commentsList = commentsRepository.findAll();
+        System.out.println("commentsList >>> " + commentsList.toString());
+        assertThat(commentsList.size()).isEqualTo(1);
+        assertThat(commentsList.get(0)).isEqualTo(comments2);
     }
 }
