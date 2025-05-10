fac1a3a54b2d14b74fa5379c10a30e71f01288c9
WooyoungKwon <rnjsdndud00@naver.com>
2025-05-10 22:44:36 +0900

    CCS-36 refactor: Redis에서 Board 정보를 가져오는 메서드 Extract

--- backend/src/main/java/com/trend_now/backend/board/application/BoardRedisService.java
+++ backend/src/main/java/com/trend_now/backend/board/application/BoardRedisService.java
@@ -4,6 +4,7 @@ import com.trend_now.backend.board.dto.BoardInfoDto;
 import com.trend_now.backend.board.dto.BoardPagingRequestDto;
 import com.trend_now.backend.board.dto.BoardPagingResponseDto;
 import com.trend_now.backend.board.dto.BoardSaveDto;
+import com.trend_now.backend.board.util.BoardServiceUtil;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -26,12 +27,10 @@ public class BoardRedisService {
     private static final long KEY_LIVE_TIME = 301L;
     private static final int KEY_EXPIRE = 0;
 
-    private static final String BOARD_KEY_DELIMITER  = ":";
-    private static final int BOARD_KEY_PARTS_LENGTH = 2;
-    private static final int BOARD_NAME_INDEX = 0;
-    private static final int BOARD_ID_INDEX = 1;
+    private static final String BOARD_KEY_DELIMITER = ":";
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final BoardServiceUtil boardServiceUtil;
 
     public void saveBoardRedis(BoardSaveDto boardSaveDto, int score) {
         String key = boardSaveDto.getName() + BOARD_KEY_DELIMITER + boardSaveDto.getBoardId();
@@ -49,7 +48,7 @@ public class BoardRedisService {
 
     public void setRankValidListTime() {
         String validTime = Long.toString(
-                LocalTime.now().plusSeconds(KEY_LIVE_TIME).toSecondOfDay());
+            LocalTime.now().plusSeconds(KEY_LIVE_TIME).toSecondOfDay());
         redisTemplate.opsForValue().set(BOARD_RANK_VALID_KEY, validTime);
     }
 
@@ -60,8 +59,8 @@ public class BoardRedisService {
         }
 
         allRankKey.stream()
-                .filter(key -> !Boolean.TRUE.equals(redisTemplate.hasKey(key)))
-                .forEach(key -> redisTemplate.opsForZSet().remove(BOARD_RANK_KEY, key));
+            .filter(key -> !Boolean.TRUE.equals(redisTemplate.hasKey(key)))
+            .forEach(key -> redisTemplate.opsForZSet().remove(BOARD_RANK_KEY, key));
     }
 
     public boolean isRealTimeBoard(BoardSaveDto boardSaveDto) {
@@ -70,7 +69,7 @@ public class BoardRedisService {
     }
 
     public BoardPagingResponseDto findAllRealTimeBoardPaging(
-            BoardPagingRequestDto boardPagingRequestDto) {
+        BoardPagingRequestDto boardPagingRequestDto) {
         Set<String> allBoardName = redisTemplate.opsForZSet().range(BOARD_RANK_KEY, 0, -1);
 
         if (allBoardName == null || allBoardName.isEmpty()) {
@@ -78,29 +77,14 @@ public class BoardRedisService {
         }
 
         List<BoardInfoDto> boardInfoDtos = allBoardName.stream()
-                .map(boardKey -> {
-
-                    // boardId 추출
-                    String[] parts = boardKey.split(BOARD_KEY_DELIMITER);
-                    log.info("[BoardRedisService.findAllRealTimeBoardPaging] : parts = {}", Arrays.toString(parts));
-
-                    // 데이터 타입 이상 시, 다음으로 넘김
-                    if(parts.length < BOARD_KEY_PARTS_LENGTH) return null;
-
-                    String boardName = parts[BOARD_NAME_INDEX];
-                    Long boardId = Long.parseLong(parts[BOARD_ID_INDEX]);
-
-                    Long boardLiveTime = redisTemplate.getExpire(boardKey, TimeUnit.SECONDS);
-                    Double score = redisTemplate.opsForZSet().score(BOARD_RANK_KEY, boardKey);
-                    return new BoardInfoDto(boardId, boardName, boardLiveTime, score);
-                })
-                .filter(Objects::nonNull)
-                .sorted(Comparator.comparingLong(BoardInfoDto::getBoardLiveTime).reversed()
-                        .thenComparingDouble(BoardInfoDto::getScore))
-                .collect(Collectors.toList());
+            .map(boardServiceUtil.getStringBoardInfoDto())
+            .filter(Objects::nonNull)
+            .sorted(Comparator.comparingLong(BoardInfoDto::getBoardLiveTime).reversed()
+                .thenComparingDouble(BoardInfoDto::getScore))
+            .collect(Collectors.toList());
 
         PageRequest pageRequest = PageRequest.of(boardPagingRequestDto.getPage(),
-                boardPagingRequestDto.getSize());
+            boardPagingRequestDto.getSize());
         int start = (int) pageRequest.getOffset();
         int end = Math.min(start + pageRequest.getPageSize(), boardInfoDtos.size());
 
