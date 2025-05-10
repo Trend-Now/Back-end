fdb07401ac9d648408231d89aaa0301d179f160c
WooyoungKwon <rnjsdndud00@naver.com>
2025-05-10 22:48:07 +0900

    CCS-36 refactor: DTO 네이밍 변경

--- backend/src/main/java/com/trend_now/backend/board/dto/BoardInfoDto.java
+++ backend/src/main/java/com/trend_now/backend/board/dto/BoardInfoDto.java
@@ -1,9 +1,11 @@
 package com.trend_now.backend.board.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class BoardInfoDto {
 
