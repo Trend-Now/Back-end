575a414f6e85c31278a04365e4e8e3df0868f3f1
WooyoungKwon <rnjsdndud00@naver.com>
2025-05-10 22:52:00 +0900

    CCS-36 feat: 검색 결과에 고정게시판, 고정게시글 추가

--- backend/src/main/java/com/trend_now/backend/board/repository/BoardRepository.java
+++ backend/src/main/java/com/trend_now/backend/board/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.trend_now.backend.board.repository;
 
+import com.trend_now.backend.board.domain.BoardCategory;
 import com.trend_now.backend.board.domain.Boards;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BoardRepository extends JpaRepository<Boards, Long> {
 
     Optional<Boards> findByName(String name);
+
+    List<Boards> findByNameLikeAndBoardCategory(String name, BoardCategory boardCategory);
+
 }
