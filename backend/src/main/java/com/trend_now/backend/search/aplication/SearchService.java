1ef0f51242e065383cae12b9a239b5bff39c4b68
kwon-wooyoung <rnjsdndud00@naver.com>
2025-06-17 22:28:25 +0900

    refactor: 검색 결과 조회 API 변경, 고정 게시판 카테고리 별로 구분해서 응답

--- backend/src/main/java/com/trend_now/backend/search/aplication/SearchService.java
+++ backend/src/main/java/com/trend_now/backend/search/aplication/SearchService.java
@@ -2,18 +2,20 @@ package com.trend_now.backend.search.aplication;
 
 import com.trend_now.backend.board.cache.BoardCacheEntry;
 import com.trend_now.backend.board.cache.RealTimeBoardCache;
-import com.trend_now.backend.board.domain.BoardCategory;
 import com.trend_now.backend.board.domain.Boards;
 import com.trend_now.backend.board.dto.BoardSummaryDto;
 import com.trend_now.backend.board.repository.BoardRepository;
 import com.trend_now.backend.post.application.PostLikesService;
-import com.trend_now.backend.post.domain.Posts;
+import com.trend_now.backend.post.dto.PostListResponseDto;
 import com.trend_now.backend.post.dto.PostSummaryDto;
+import com.trend_now.backend.post.dto.PostWithBoardSummaryDto;
+import com.trend_now.backend.post.dto.RealtimePostSearchDto;
 import com.trend_now.backend.post.repository.PostsRepository;
 import com.trend_now.backend.search.dto.AutoCompleteDto;
 import com.trend_now.backend.search.dto.SearchResponseDto;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -45,39 +47,47 @@ public class SearchService {
      */
     public SearchResponseDto findBoardAndPostByKeyword(String keyword, int page, int size) {
         Pageable pageable = PageRequest.of(page, size,
-            Sort.by(Sort.Order.desc("updatedAt"), Sort.Order.desc("createdAt")));
+            Sort.by(Sort.Order.desc("createdAt"), Sort.Order.desc("updatedAt")));
         // 현재 인메모리에 캐싱된 데이터 조회
-        Map<Long, BoardCacheEntry> boardCacheEntryMap = realTimeBoardCache.getBoardCacheEntryMap().asMap();
-        Map<Long, BoardCacheEntry> fixedBoardCacheMap = realTimeBoardCache.getFixedBoardCacheMap().asMap();
+        Map<Long, BoardCacheEntry> boardCacheEntryMap = realTimeBoardCache.getBoardCacheEntryMap()
+            .asMap();
+        Map<Long, BoardCacheEntry> fixedBoardCacheMap = realTimeBoardCache.getFixedBoardCacheMap()
+            .asMap();
 
         // 실시간 인기 게시판 중, 키워드가 포함된 게시판만 필터링
         List<BoardSummaryDto> filteredKeywords = filterBoardsByKeyword(keyword, boardCacheEntryMap);
 
         // 실시간 게시판에 해당하는(타이머가 남아있는) 게시글 중, 내용 또는 제목에 keyword가 포함된 게시글 조회
-        List<PostSummaryDto> postSummaryDtoList = filterRealTimePostsByKeyword(keyword, boardCacheEntryMap.keySet(), pageable);
+        Page<PostWithBoardSummaryDto> postWithBoardSummaryList = filterRealTimePostsByKeyword(
+            keyword, boardCacheEntryMap.keySet(), pageable);
+        RealtimePostSearchDto realtimePostSearchDto = RealtimePostSearchDto.of(
+            postWithBoardSummaryList.getTotalPages(), postWithBoardSummaryList.getTotalElements(),
+            postWithBoardSummaryList.getContent());
 
         // 고정 게시판 조회
-        List<BoardSummaryDto> fixedBoardTitleList = filterBoardsByKeyword(keyword, fixedBoardCacheMap);
+        List<BoardSummaryDto> fixedBoardTitleList = filterBoardsByKeyword(keyword,
+            fixedBoardCacheMap);
 
         // 고정 게시판에서 게시글 조회
-        List<PostSummaryDto> fixedPostSummaryList = filterFixedPostsByKeyword(keyword, pageable);
+        Map<String, PostListResponseDto> fixPostSearchMap = filterFixedPostsByKeyword(
+            keyword,
+            fixedBoardCacheMap, pageable);
 
         log.info("{} 검색 성공, 결과를 반환합니다", keyword);
 
         return SearchResponseDto.builder()
             .message("게시글 검색 조회 성공")
-            .boardList(filteredKeywords)
-            .postList(postSummaryDtoList)
+            .realtimeBoardList(filteredKeywords)
+            .realtimePostList(realtimePostSearchDto)
             .fixedBoardList(fixedBoardTitleList)
-            .fixedPostList(fixedPostSummaryList)
+            .fixedPostList(fixPostSearchMap)
             .build();
     }
 
-    private List<PostSummaryDto> filterRealTimePostsByKeyword(String keyword,
+    private Page<PostWithBoardSummaryDto> filterRealTimePostsByKeyword(String keyword,
         Set<Long> boardCacheIdList, Pageable pageable) {
-        Page<Posts> posts = postsRepository.findByKeywordAndRealTimeBoard(keyword, boardCacheIdList,
+        return postsRepository.findByKeywordAndRealTimeBoard(keyword, boardCacheIdList,
             pageable);
-        return convertToPostSummaryDtos(posts);
     }
 
     private List<BoardSummaryDto> filterBoardsByKeyword(String keyword,
@@ -97,20 +107,26 @@ public class SearchService {
             .toList();
     }
 
-    private List<PostSummaryDto> filterFixedPostsByKeyword(String keyword, Pageable pageable) {
+    private Map<String, PostListResponseDto> filterFixedPostsByKeyword(String keyword,
+        Map<Long, BoardCacheEntry> fixedBoardCacheMap, Pageable pageable) {
 
-        Page<Posts> fixedPostsPage = postsRepository.findByBoardCategoryAndKeyword(keyword,
-            BoardCategory.FIXED, pageable);
-        return convertToPostSummaryDtos(fixedPostsPage);
-    }
-
-    private List<PostSummaryDto> convertToPostSummaryDtos(Page<Posts> byFixedBoardAndKeyword) {
-        return byFixedBoardAndKeyword.getContent().stream()
-            .map(post -> {
-                int postLikesCount = postLikesService.getPostLikesCount(post.getBoards().getId(),
-                    post.getId());
-                return PostSummaryDto.of(post, postLikesCount);
-            }).toList();
+        Map<String, PostListResponseDto> fixBoardList = new HashMap<>();
+        /**
+         * 캐싱 되어 있는 고정 게시판 목록에 속한 게시글 중, 내용 또는 제목에 keyword가 포함된 게시글을 조회합니다.
+         */
+        for (Long fixBoardId : fixedBoardCacheMap.keySet()) {
+            // fixBoard에 속한 게시글 중, 내용 또는 제목에 keyword가 포함된 게시글 조회
+            Page<PostSummaryDto> postSummaryDtoList = postsRepository.findByFixBoardsAndKeyword(
+                keyword, fixBoardId, pageable);
+            String boardName = fixedBoardCacheMap.get(fixBoardId).getBoardName();
+            fixBoardList.put(
+                boardName,
+                PostListResponseDto.of(boardName + " 게시글 검색 결과",
+                    postSummaryDtoList.getTotalPages(),
+                    postSummaryDtoList.getTotalElements(),
+                    postSummaryDtoList.getContent()));
+        }
+        return fixBoardList;
     }
 
     // 게시판 이름 자동완성 메서드
@@ -118,9 +134,11 @@ public class SearchService {
         // 공백 제거
         String trimmedPrefix = prefix.replaceAll(" ", "");
         // 캐싱해놓은 실시간 인기 검색어 리스트 조회
-        Map<Long, BoardCacheEntry> boardCacheEntryMap = realTimeBoardCache.getBoardCacheEntryMap().asMap();
+        Map<Long, BoardCacheEntry> boardCacheEntryMap = realTimeBoardCache.getBoardCacheEntryMap()
+            .asMap();
         // 캐싱해놓은 고정 게시판 리스트 조회
-        Map<Long, BoardCacheEntry> fixedBoardCacheMap = realTimeBoardCache.getFixedBoardCacheMap().asMap();
+        Map<Long, BoardCacheEntry> fixedBoardCacheMap = realTimeBoardCache.getFixedBoardCacheMap()
+            .asMap();
 
         List<AutoCompleteDto> filteredBoards = boardCacheEntryMap.entrySet().stream()
             .filter(fixedBoard -> fixedBoard.getValue().getBoardName()
