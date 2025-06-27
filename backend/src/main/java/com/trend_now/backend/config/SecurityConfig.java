7723b9a4f8200f5b01c9bd1bdef6782e03598cc1
kwon-wooyoung <rnjsdndud00@naver.com>
2025-06-27 19:50:09 +0900

    feat: CORS 프론트 도메인 주소 추가

--- backend/src/main/java/com/trend_now/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/trend_now/backend/config/SecurityConfig.java
@@ -74,7 +74,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-            Arrays.asList("http://localhost:3000", "http://localhost:63342"));    // 개발 프론트엔드 도메인 허용
+            Arrays.asList("http://localhost:3000", "http://localhost:63342",
+                "https://www.trendnow.me",
+                "https://front-end-git-bugfix-login-trendnow.vercel.app",
+                "https://front-end-git-dev-trendnow.vercel.app"));    // 개발 프론트엔드 도메인 허용
         configuration.setAllowedMethods(Arrays.asList("*"));    // 모든 HTTP 메서드 허용
         configuration.setAllowedHeaders(Arrays.asList("*"));    // 모든 헤더 값 허용
         configuration.setAllowCredentials(true);    // 자격 증명을 허용(Authorization 헤더를 허용 목적)
