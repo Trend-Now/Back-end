68dbd44ffb95d20ea59422cd323de5b9f7d7790b
kwon-wooyoung <rnjsdndud00@naver.com>
2025-09-01 15:56:41 +0900

    CCS-68 feat: 네이버 뉴스 내용 요약을 위한 GEMINI API 요청 추가

---
+++ backend/src/main/java/com/trend_now/backend/keyword_summarize/GeminiService.java
@@ -0,0 +1,39 @@
+package com.trend_now.backend.keyword_summarize;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import com.trend_now.backend.keyword_summarize.dto.NaverNewsResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeminiService {
+
+    public static final String SUMMARIZE_REQUEST_PROMPT = """
+        ' %s '는 현재 대한민국 실시간 인기 검색어입니다. 아래 최근 뉴스 기사들을 참고하여, ' %s '가 왜 인기 검색어에 올랐는지 5문장 이내로 요약해 주세요.
+        뉴스 기사: %s
+        """;
+    public static final String GEMINI_MODEL_NAME = "gemini-2.5-flash-lite";
+
+    private final Client client;
+    private final NaverApiService naverApiService;
+
+    public GeminiService(@Value("${google.gemini.api.key}") String apiKey, NaverApiService naverApiService) {
+        this.client = Client.builder()
+            .apiKey(apiKey).build();
+        this.naverApiService = naverApiService;
+
+    }
+
+    public String summarizeKeyword(String keyword) {
+        NaverNewsResponseDto naverNewsResponseDto = naverApiService.searchNewsByKeyword(keyword);
+        String prompt = String.format(SUMMARIZE_REQUEST_PROMPT, keyword, keyword, naverNewsResponseDto.getItems());
+        GenerateContentResponse response =
+            client.models.generateContent(
+                GEMINI_MODEL_NAME,
+                prompt,
+                null);
+
+        return response.text();
+    }
+}
