982040c6e2a057ca4f952cdfcc8379249f7a2fcc
wooyoung kwon <rnjsdndud00@cruxdata.co.kr>
2025-11-28 15:01:41 +0900

    feat: 실시간 스케줄러에 opensearch 적용

--- backend/src/main/java/com/trend_now/backend/board/application/BoardService.java
+++ backend/src/main/java/com/trend_now/backend/board/application/BoardService.java
@@ -105,11 +105,11 @@ public class BoardService {
     }
 
     @Transactional
-    public void updateBoardName(BoardSaveDto boardSaveDto) {
-        Boards board = boardRepository.findById(boardSaveDto.getBoardId())
-            .orElseThrow(() -> new NotFoundException(BOARD_NOT_FOUND_MESSAGE + boardSaveDto.getBoardName()));
-        if (!boardRepository.existsByName(boardSaveDto.getBoardName())) {
-            board.updateName(boardSaveDto.getBoardName());
+    public void updateBoardName(Long boardId, String newBoardName) {
+        Boards board = boardRepository.findById(boardId)
+            .orElseThrow(() -> new NotFoundException(BOARD_NOT_FOUND_MESSAGE + newBoardName));
+        if (!boardRepository.existsByName(newBoardName)) {
+            board.updateName(newBoardName);
         }
     }
 
